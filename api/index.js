27b239e5812714e942aa7da712eceff66c265b07
devgairola910 <dev_ashish2022@outlook.com>
2026-08-30 21:18:52 +0530

    refactor: update Vercel configuration for serverless functions and disable bodyParser in API handler

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import handler from "../server.js";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function apiHandler(request, response) {
   return handler(request, response);
 }
